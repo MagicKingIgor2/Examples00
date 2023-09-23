e535cf68a6f75402cc54ad44611bafd37c434012
Igor <gosha99_2010@inbox.ru>
2023-09-23 08:25:04 +0300

    Добавил в проект возможные вариации имени любимчика

--- Example006_Priority/Program.cs
+++ Example006_Priority/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выбираю любимчика
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша"){
+    Console.WriteLine("Ура, это же МАША!");
+}
+else {
+    Console.WriteLine("Привет, !");
+    Console.WriteLine(username);
+}
