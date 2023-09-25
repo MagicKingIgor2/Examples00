926be573a7d0625da8f4be278c5f8256a052ec66
Igor <gosha99_2010@inbox.ru>
2023-09-25 19:02:03 +0300

    добавил методы заполнения, печати массива и поиск номера элемента, содержащего нужное значение

---
+++ Example010_ArrayPlus/Program.cs
@@ -0,0 +1,42 @@
+﻿// Модифицированный поиск элеметов в массиве
+// в котором идет рандомный подбор элементов
+void FillArray(int[] collection) {
+    int length = collection.Length; // .length  - длина (количество элеметов) в массиве
+    int index = 0;
+    while (index < length) {
+        collection[index] = new Random().Next(1, 15);
+        index++;
+    }
+}
+// выше представлен метод заполнения массива рандомными числами от 1 до 14 включительно
+void PrintArray(int[] col) {
+    int count = col.Length;
+    int position = 0;
+    while (position < count) {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+// выше представлен метод печати массива, который был заполнен рандомно 
+int IndexOf(int[] collection, int find) {
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count) {
+        if (collection[index] == find) {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+// описан метод нахождения позиции искомого значения в массиве
+int[] array = new int[15]; // new int - функция создания массива из 15 элементов, заполненные 0 по умолчанию
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 777);
+Console.WriteLine(pos);
