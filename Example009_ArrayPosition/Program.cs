c5b1b7532ef4879678abb624b3fbfea8e8908547
Igor <gosha99_2010@inbox.ru>
2023-09-25 18:15:54 +0300

    Добавил проект массива, в котором осуществляется поиск нужного элемента и выводит его содержимое

--- Example009_ArrayPosition/Program.cs
+++ Example009_ArrayPosition/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Имеется одномерный массив из n элементов,
+// требуется найти элемент массива, равный find
+int[] array = {1, 2 , 3, 44, 36, 47, 8, 569, 4, 78, 0, 7};
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n) {
+    if (array[index] == find) {
+        Console.WriteLine(index);
+        break; // завершает программу, в случае первого совпадения
+    }
+    index++;
+}
